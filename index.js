673a2d4d8320a587a417fa735ff4a9557ddbe576
Megha Gheedia <73075314+megha-105@users.noreply.github.com>
2023-07-21 15:32:11 +0530

    update

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-let set=[1,2,3,4,5,6];
-var p1=Math.floor(Math.random(1,7)*10);
-var p2=Math.floor(Math.random(1,7)*10);
+
+var p1=Math.floor(Math.random()*6)+1;
+var p2=Math.floor(Math.random()*6)+1;
+
+var i1="images/dice"+p1+".png";
+var i2="images/dice"+p2+".png";
+
 
 if(p1>p2)
 {
     document.querySelector("h1").textContent="Player 1 Wins";
-    document.querySelector(".img1").setAttribute("src","images/dice6.png");
-    document.querySelector(".img2").setAttribute("src","images/dice1.png");
+    document.querySelector(".img1").setAttribute("src",i1);
+    document.querySelector(".img2").setAttribute("src",i2);
 
 }
 else if(p2>p1){
     document.querySelector("h1").textContent="Player 2 Wins";
-    document.querySelector(".img2").setAttribute("src","images/dice5.png");
-    document.querySelector(".img1").setAttribute("src","images/dice4.png");
+    document.querySelector(".img2").setAttribute("src",i2);
+    document.querySelector(".img1").setAttribute("src",i1);
 
 }
 else{
     document.querySelector("h1").textContent="Draw";
-        document.querySelector(".img1").setAttribute("src","images/dice3.png");
-        document.querySelector(".img2").setAttribute("src","images/dice3.png");
+        document.querySelector(".img1").setAttribute("src",i1);
+        document.querySelector(".img2").setAttribute("src",i2);
 }
